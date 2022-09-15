3e56b7045b31f794431fbc742368721e5e8ee42e
Shengjiang Quan <qsj287068067@126.com>
2022-09-15 22:32:17 +0900

    bts r50 city: now can train normally
    1. use `max_depth=80` as in kitti. This is the maximum distance
    of samples, which is dataset specific
    2. repeat cityscapes by 8 times
    
    Signed-off-by: Shengjiang Quan <qsj287068067@126.com>

--- configs/semcl/bts_r50_city_24e.py
+++ configs/semcl/bts_r50_city_24e.py
@@ -16,14 +16,54 @@ model = dict(
     decode_head=dict(
         final_norm=False,
         min_depth=1e-3,
-        max_depth=10,
+        max_depth=80, # this is dataset decided, use 80 as in kitti
         loss_decode=dict(
             type='SigLoss', valid_mask=True, loss_weight=1.0)),
     )
-
+    
+dataset_type = 'CSsemclDataset'
+data_root = 'data/cityscapes'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='DisparityLoadAnnotations'),
+    dict(type='Resize', img_scale=(1216, 352), keep_ratio=False),
+    dict(type='KBCrop', depth=True),
+    dict(type='RandomRotate', prob=0.5, degree=2.5),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='RandomCrop', crop_size=(352, 704)),
+    dict(type='ColorAug', prob=1, gamma_range=[0.9, 1.1], brightness_range=[0.9, 1.1], color_range=[0.9, 1.1]),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', 
+         keys=['img', 'depth_gt'], 
+         meta_keys=('filename', 'ori_filename', 'ori_shape',
+                    'img_shape', 'pad_shape', 'scale_factor', 
+                    'flip', 'flip_direction', 'img_norm_cfg',
+                    'cam_intrinsic')),
+]
 data = dict(
     samples_per_gpu=8, # batchsize=16 on a dual-gpu node
     workers_per_gpu=8,
+    train=dict(
+        type='RepeatDataset',
+        times=8,
+        dataset=dict(
+            type=dataset_type,
+            data_root=data_root,
+            img_dir='leftImg8bit',
+            cam_dir='camera',
+            ann_dir='disparity',
+            depth_scale=256,
+            split='cityscapes_train.txt',
+            pipeline=train_pipeline,
+            garg_crop=True,
+            eigen_crop=False,
+            min_depth=1e-3,
+            max_depth=200
+        )
+    )
 )
 
 # find_unused_parameters=True
--- depth/datasets/__init__.py
+++ depth/datasets/__init__.py
@@ -3,11 +3,13 @@ from .kitti import KITTIDataset
 from .nyu import NYUDataset
 from .sunrgbd import SUNRGBDDataset
 from .custom import CustomDepthDataset
+from .dataset_wrappers import (ConcatDataset,
+                               RepeatDataset)
 from .cityscapes import CSDataset
 from .cityscapes_semcl import CSsemclDataset
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .nyu_binsformer import NYUBinFormerDataset
 
 __all__ = [
-    'KITTIDataset', 'NYUDataset', 'SUNRGBDDataset', 'CustomDepthDataset', 'CSDataset', 'CSsemclDataset', 'NYUBinFormerDataset'
+    'KITTIDataset', 'NYUDataset', 'SUNRGBDDataset', 'CustomDepthDataset', 'CSDataset', 'CSsemclDataset', 'NYUBinFormerDataset', 'ConcatDataset', 'RepeatDataset'
 ]
