9ed9cd777f700adc931731b1b489ff959d7e89f9
Shengjiang Quan <qsj287068067@126.com>
2022-09-27 12:21:40 +0900

    dist_train.sh: add variables for multi-machine distributed training
    Signed-off-by: Shengjiang Quan <qsj287068067@126.com>

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -3,7 +3,10 @@
 CONFIG=$1
 GPUS=$2
 PORT=${PORT:-38423}
+MASTER_ADDR=${MASTER_ADDR:-localhost}
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --nnodes=$NNODES --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
