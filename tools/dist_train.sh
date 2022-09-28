2c94ae66504e6cd1c910e71cb096a0d6cf06a566
Shengjiang Quan <qsj287068067@126.com>
2022-09-28 19:51:00 +0900

    print distributed settings
    Signed-off-by: Shengjiang Quan <qsj287068067@126.com>

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -7,6 +7,9 @@ MASTER_ADDR=${MASTER_ADDR:-localhost}
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
 
+echo "Distributed: $MASTER_ADDR:$PORT"
+echo "rank: $NODE_RANK/$NNODES"
+
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --nnodes=$NNODES --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
